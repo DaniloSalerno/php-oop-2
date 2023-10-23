a8a9aea26ae3a06ff267102dde610496f9bb365b
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-23 15:47:51 +0200

    change exception

--- traits/price.php
+++ traits/price.php
@@ -8,17 +8,17 @@ trait Price
 
             if ($_price < 0.1) {
 
-                throw new Exception('The value of price is too low,inser a value superior of 0.1');
+                throw new RangeException('The value of price is too low,inser a value superior of 0.1');
             } else if ($_price > 999) {
 
-                throw new Exception('The value of price is too hight,inser a value inferior of 999');
+                throw new RangeException('The value of price is too hight,inser a value inferior of 999');
             } else {
 
                 $this->price = $_price;
             }
         } else {
 
-            throw new Exception('The value of price is not valid,insert a number value');
+            throw new InvalidArgumentException('The value of price is not valid,insert a number value');
         }
     }
 
