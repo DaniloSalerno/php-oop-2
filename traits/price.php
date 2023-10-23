015c440694568cbac99786a2c40e222357244642
DaniloSalerno <salerno.danilo@hotmail.it>
2023-10-23 15:32:32 +0200

    add exception in price.php

--- traits/price.php
+++ traits/price.php
@@ -4,7 +4,22 @@ trait Price
 {
     function setPrice($_price)
     {
-        $this->price = $_price;
+        if (is_numeric($_price)) {
+
+            if ($_price < 0.1) {
+
+                throw new Exception('The value of price is too low,inser a value superior of 0.1');
+            } else if ($_price > 999) {
+
+                throw new Exception('The value of price is too hight,inser a value inferior of 999');
+            } else {
+
+                $this->price = $_price;
+            }
+        } else {
+
+            throw new Exception('The value of price is not valid,insert a number value');
+        }
     }
 
     function getPrice()
